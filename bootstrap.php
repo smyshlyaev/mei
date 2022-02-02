0c6135643a7685caf76f29222ec8054e67fa90e6
a.smyshlyaev <st105t.mk@gmail.com>
2022-02-02 22:44:52 +0300

    Add exceptions

--- bootstrap.php
+++ bootstrap.php
@@ -7,18 +7,24 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $manager = new Manager();
-
-$requisites = [
-    'driver' => $_ENV['DB_DRIVER'],
-    'host' => $_ENV['DB_HOST'],
-    'database' => $_ENV['DB_DATABASE'],
-    'username' => $_ENV['DB_USERNAME'],
-    'password' => $_ENV['DB_PASSWORD'],
-    'charset' => $_ENV['DB_CHARSET'],
-    'collation' => $_ENV['DB_COLLATION'],
-    'prefix' => $_ENV['DB_PREFIX'],
-];
-$manager->addConnection($requisites);
-
+try {
+    $requisites = [
+        'driver' => $_ENV['DB_DRIVER'],
+        'host' => $_ENV['DB_HOST'],
+        'database' => $_ENV['DB_DATABASE'],
+        'username' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'charset' => $_ENV['DB_CHARSET'],
+        'collation' => $_ENV['DB_COLLATION'],
+        'prefix' => $_ENV['DB_PREFIX'],
+    ];
+} catch(\Throwable $e) {
+    throw new Exception('Заполните реквизиты в .env-файле');
+}
+try {
+    $manager->addConnection($requisites);
+} catch(\Throwable $e) {
+    throw new Exception('Ошибка подключения к базе данных');
+}
 $manager->setAsGlobal();
 $manager->bootEloquent();
--- repositories/TableRepository.php
+++ repositories/TableRepository.php
@@ -9,6 +9,10 @@ class TableRepository implements TableRepositoryInterface
 {
     public function create($item)
     {
-        return Table::Create($item);
+        try {
+            return Table::Create($item);
+        } catch(\Throwable $e) {
+            throw new \Exception('Ошибка во время записи в базу. Проверьте правильность формата данных.');
+        }
     }
 }
